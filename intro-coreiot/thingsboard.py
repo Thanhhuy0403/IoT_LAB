d5f5aa2dc2f3335ff21431e5d717be1b6cc4043f
Nguyen Hao Khang <nguyenhaokhang2005@gmail.com>
2026-04-17 01:30:41 +0700

    Week1: Fix the error of accidentally deleting code.

--- intro-coreiot/thingsboard.py
+++ intro-coreiot/thingsboard.py
@@ -52,3 +52,17 @@ light_intesity = 100
 #HCMUT
 long = 106.65789107082472
 lat = 10.772175109674038
+
+#H6
+# long = 106.80633605864662
+# lat = 10.880018410410052
+
+while True:
+    collect_data = {'temperature': temp, 'humidity': humi,
+                    'light':light_intesity,
+                    'long': long, 'lat': lat}
+    temp += 1
+    humi += 1
+    light_intesity += 1
+    client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
+    time.sleep(5)
--- intro-coreiot/thingsboard2.py
+++ intro-coreiot/thingsboard2.py
@@ -50,5 +50,26 @@ humi = 50
 light_intesity = 100
 counter = 0
 
-#HCMUT
+# #HCMUT
 # long = 106.65789107082472
+# lat = 10.772175109674038
+
+#H6
+long = 106.80633605864662
+lat = 10.880018410410052
+
+while True:
+    collect_data = {'temperature': temp, 'humidity': humi,
+                    'light':light_intesity,
+                    'long': long, 'lat': lat}
+    temp += 1
+    humi += 1
+    light_intesity -= 1
+
+    if temp == 100:
+        temp = 30
+        humi = 50
+        light_intesity = 100
+
+    client.publish('v1/devices/me/telemetry', json.dumps(collect_data), 1)
+    time.sleep(5)
